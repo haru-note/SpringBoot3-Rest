c1e36cf1d18b85ddd319f165d55d9a7ca9d512e0
gliderwiki <performizer@gmaill.com>
2023-01-16 16:12:07 +0900

    note 예제 초기 세팅

---
+++ src/main/java/net/harunote/spring3rest/mynote/MyNoteController.java
@@ -0,0 +1,9 @@
+package net.harunote.spring3rest.mynote;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class MyNoteController {
+}
