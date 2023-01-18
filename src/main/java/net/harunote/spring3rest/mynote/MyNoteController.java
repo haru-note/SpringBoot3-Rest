4bf704dc1360ec625e4e74368149f1792ae06de5
gliderwiki <performizer@gmaill.com>
2023-01-18 10:54:04 +0900

    MyNote Controller 작성

--- src/main/java/net/harunote/spring3rest/mynote/MyNoteController.java
+++ src/main/java/net/harunote/spring3rest/mynote/MyNoteController.java
@@ -1,9 +1,12 @@
 package net.harunote.spring3rest.mynote;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class MyNoteController {
+    Logger log = LoggerFactory.getLogger(MyNoteController.class);
 }
---
+++ src/main/java/net/harunote/spring3rest/mynote/entity/MyNoteEntity.java
@@ -0,0 +1,4 @@
+package net.harunote.spring3rest.mynote.entity;
+
+public class MyNoteEntity {
+}
