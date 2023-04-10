3756c63402d4fefbed4c7796e4bcbcbc971d333e
gliderwiki <performizer@gmail.com>
2023-04-10 16:12:54 +0900

    Swagger 샘플 추가

--- src/main/java/net/harunote/spring3rest/hello/HelloController.java
+++ src/main/java/net/harunote/spring3rest/hello/HelloController.java
@@ -1,5 +1,12 @@
 package net.harunote.spring3rest.hello;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import net.harunote.spring3rest.hello.model.Hello;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +17,18 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 @RestController
+@Tag(name = "Hello", description = "the heath check API")
 public class HelloController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @Operation(operationId = "Hello World!", summary = "Health check", description = "서버가 살아있는지 체크한다.", tags = {"Hello"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful", content = @Content(schema = @Schema(implementation = Hello.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid ID supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "ID not found", content = @Content) })
     @GetMapping("/hello")
-    public Hello helloWorld(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Hello helloWorld(@Parameter(description = "파라미터의 설명을 기입한다.", required = true) @RequestParam(value = "name", defaultValue = "World") String name) {
         return new Hello(counter.incrementAndGet(), String.format(template, name));
     }
 }
--- src/main/java/net/harunote/spring3rest/mynote/request/MyNoteRequest.java
+++ src/main/java/net/harunote/spring3rest/mynote/request/MyNoteRequest.java
@@ -1,5 +1,7 @@
 package net.harunote.spring3rest.mynote.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,11 +15,16 @@ import java.util.List;
 @Getter
 @ToString
 public class MyNoteRequest {
+    @Schema(description = "제목", example = "마이노트 제목입니다.", required = true)
+    @NotBlank
     private String title;
 
+    @Schema(description = "내용", example = "마이노트 내용입니다.", required = true)
+    @NotBlank
     private String content;
 
+    @Schema(description = "완료 여부", example = "true")
     private boolean done;
-
+    @Schema(description = "태그 리스트", example = "[ {\"deleted\" : \"false\", \"tagName\" : \"Kubernetes\"}, {\"deleted\" : \"false\", \"tagName\" : \"Docker\"} ]")
     private List<TagRequest> tag;
 }
